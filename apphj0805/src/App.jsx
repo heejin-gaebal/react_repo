f6c11f490d4695e986ac3f12b05b7de1c899483d
DESKTOP-0BS1CPD\user1 <jeonghyeonjun@naver.com>
2025-08-11 21:02:41 +0900

    리덕스 연습

--- apphj0805/src/App.jsx
+++ apphj0805/src/App.jsx
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import BoardListPage from './pages/board/BoardListPage';
 import BoardInsertPage from './pages/board/BoardInsertPage';
 import HomePage from './pages/HomePage';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <h1>app0805 복습</h1>
       <BrowserRouter>
         <Routes>
@@ -17,7 +19,7 @@ function App() {
           <Route path="*" element={<HomePage />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </Provider>
   );
 }
 
---
+++ apphj0811/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>App0811</h1>
+    </>
+  );
+}
+
+export default App;
