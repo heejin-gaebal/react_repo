3d7651c8f8a4e5f6f4de4fae559d71992dac0b73
DESKTOP-0BS1CPD\user1 <jeonghyeonjun@naver.com>
2025-09-30 16:35:50 +0900

    파일 연습

--- app31/src/pages/Homepage.jsx
+++ app31/src/pages/Homepage.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  const { formData, handleChange, setFormData } = useForm({});
+
+  const handleSubmit = () => {
+    const fd = new FormData();
+    fd.append('title', formData.title);
+    fd.append('content', formData.content);
+    fd.append('f', formData.f); //파일객체 꺼내오기
+    const url = 'http://127.0.0.1:8080/api/board';
+    const option = {
+      method: 'POST',
+      headers: {},
+      body: fd,
+    };
+    fetch(url, option).then((resp) => resp.ok()); //응답의 상태코드확인해보기
+  };
+
+  const handleFileChange = (e) => {
+    e.preventDefault;
+    console.log(e.target.files[0]); //파일 객체 확인 | 리스트형[배열]
+    const fileData = e.target.files[0];
+    setFormData((prev) => {
+      return { ...prev, f: fileData };
+    });
+  };
+
+  return (
+    <>
+      <h1>파일업로드 연습</h1>
+      <input type="text" name="title" onChange={handleChange} />
+      <br />
+      <textarea name="content" onChange={handleChange}></textarea>
+      <br />
+      <input type="file" multiple name="f" onChange={handleFileChange} />
+      <br />
+      <input type="button" onClick={handleSubmit} value={'제출하기'} />
+    </>
+  );
 };
 
 export default Homepage;
